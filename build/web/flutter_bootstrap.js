a0f456fd65570e1c8618b83a5df1d4e7290754a8
raider9981-glitch <raider9981@gmail.com>
2026-08-21 14:15:49 +0800

    Fix GitHub Pages base href

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"5d531788691ec3404cac0cee66ead4007b1773
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "564266784" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2279678215" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
