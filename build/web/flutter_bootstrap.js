773a2838e9029be0f3cf5a6233b587ae4ee84d5d
raider9981-glitch <raider9981@gmail.com>
2026-08-21 08:59:52 +0800

    修正 GitHub Pages 路徑

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"5d531788691ec3404cac0cee66ead4007b1773
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "893724519" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2459062424" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
